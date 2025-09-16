1496378c2eab007a07dd7bc38b11a5d6fc4c48f6
AugustoFerrer <gutoprogramas05@gmail.com>
2025-09-16 20:00:01 -0300

    Update tratamento_1.py

--- Pesquisa/tratamento_1.py
+++ Pesquisa/tratamento_1.py
@@ -2,7 +2,7 @@ import nltk
 from nltk.corpus import stopwords
 import re 
 import pandas as pd
-nltk.download('stopwords')
+
 df = pd.read_csv(r"C:\Users\Lenovo\Desktop\Workspace\estudos\Pesquisa-ADO\Dataset.csv", encoding='utf-8', low_memory=False)
 
 # Selecionar apenas as duas colunas principais (text + hatespech_comb)
@@ -18,4 +18,5 @@ def remover_stopwords(texto):
 df['text'] = df['text'].apply(remover_stopwords)
 print(df.head(10))
 
-df.to_csv("dataset_tratado_1",index = False, encoding='utf-8')
+df.to_csv("dataset_tratado_1.csv",index = False, encoding='utf-8')
+print("ve se mudou ai")
