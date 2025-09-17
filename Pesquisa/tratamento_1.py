4b0fcecf1af47de14a4651b662618937f7f86c33
AugustoFerrer <gutoprogramas05@gmail.com>
2025-09-17 15:24:28 -0300

    união dos tratamentos gerando 1 dataset

--- Pesquisa/tratamento_1.py
+++ Pesquisa/tratamento_1.py
@@ -2,6 +2,7 @@ import nltk
 from nltk.corpus import stopwords
 import re 
 import pandas as pd
+import string
 
 df = pd.read_csv(r"C:\Users\Lenovo\Desktop\Workspace\estudos\Pesquisa-ADO\Dataset.csv", encoding='utf-8', low_memory=False)
 
@@ -18,5 +19,28 @@ def remover_stopwords(texto):
 df['text'] = df['text'].apply(remover_stopwords)
 print(df.head(10))
 
-df.to_csv("dataset_tratado_1.csv",index = False, encoding='utf-8')
-print("ve se mudou ai")
+
+
+# Esse tratamento está focando em retirar espaços em excesso e pontuação.
+
+# print("Informações do dataset: ")
+# print(df.info)
+
+# print("\nPrimeiros 5 registros")
+# print(df.head)
+
+# Remover a pontuação
+
+def remover_pontuacao(texto):
+    return texto.translate(str.maketrans('','',string.punctuation))
+
+df['text'] = df['text'].astype(str).apply(remover_pontuacao)
+
+# Remover espaços em excesso
+
+df['text'] = df['text'].apply(lambda x: " ".join(x.split()))
+
+# print("\nPrimeiros 5 registros após a remoção de pontuação: ")
+# print(df.head())
+
+df.to_csv("dataset_tratado_final.csv", index=False, encoding='utf-8')
