eb5c78cab17e52c0e9df5fd370c35621365a2eca
aiko <122801456+aikoian@users.noreply.github.com>
2025-10-11 20:08:33 -0300

    algumas mudanças
    estou tentando avaliar a nossa escolha do dataset, o apontamento do professor foi muito preciso, e ao ler mais linhas do nosso dataset encontrei diversas incogruências. Mas tive uma ideia bacana de pegar outro dataset pra tentar refazer todo o processo de pre-processing que já foi feito pra ver se o resultado fica mais satisfatório.

---
+++ Novo dataset/juntar_datasets.py
@@ -0,0 +1,13 @@
+"""
+links importantes:
+
+link do dataset OLID-BR: https://www.kaggle.com/datasets/dougtrajano/olidbr?resource=download
+link do dataset HateBR : https://github.com/franciellevargas/HateBR/blob/main/dataset/HateBR.csv
+
+"""
+def linha():
+    print("-="*115)
+    print()
+
+# minha ideia era fazer a junção de dois datasets, porém me encontrei numa confunsão absurda, mas acabei chegando em uma solução melhor
+
